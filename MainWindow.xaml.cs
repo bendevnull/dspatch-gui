a9427e772bec81dfe4e7a4ff8f9d0c0a6ebc2696
bennyman123abc <bennyman123abc@hotmail.com>
2017-12-12 20:59:24 -0500

    Initial Commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.IO;
+using dspatch;
+using dspatch.Nitro;
+
 
 namespace dspatch_gui
 {
@@ -23,6 +29,121 @@ namespace dspatch_gui
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        static byte[] dsHash = //Hash of the DS Download Station ROM
+        {
+            0xF1, 0x8B, 0x55, 0xF3, 0xE1, 0x25, 0x9C, 0x03, 0xE1, 0x0D, 0x0E, 0xCB,
+            0x54, 0x96, 0x93, 0xB4, 0x29, 0x05, 0xCE, 0xB5
+        };
+
+        private Boolean checkHash(string rom)
+        {
+            if (rom == "")
+            {
+                return false;
+            }
+            byte[] dsdata = File.ReadAllBytes(rom);
+            byte[] sha1 = SHA1.Create().ComputeHash(dsdata);
+            for (int i = 0; i < 20; i++)
+            {
+                if (sha1[i] != dsHash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private Boolean createROM()
+        {
+            // Make sure everything is filled out properly
+            if (checkHash(downloadStationTextBox.Text) == false)
+            {
+                MessageBox.Show("You aren't using the correct DS Download Station ROM!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (outputTextBox.Text == "")
+            {
+                MessageBox.Show("You did not assign a location for the patched ROM to be saved to!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (romListBox.HasItems == false)
+            {
+                MessageBox.Show("You did not add any ROMs to include in the patched DS Download Station!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            byte[] dsdata = File.ReadAllBytes(downloadStationTextBox.Text);
+            DownloadStationPatcher p = new DownloadStationPatcher(new NDS(dsdata));
+            foreach(var r in romListBox.Items)
+                p.AddRom(new NDS(File.ReadAllBytes(r.ToString())));
+            byte[] finalResult = p.ProduceRom().Write();
+            File.Create(outputTextBox.Text).Close();
+            File.WriteAllBytes(outputTextBox.Text, finalResult);
+            return true;
+        }
+
+        private void bar_Credits_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 credits = new Window1();
+            credits.Show();
+        }
+
+        private void downloadStationROMOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "All Usable Files (*.nds;*.srl)|*.nds;*.srl|All Files (*.*)|*.*";
+            if (open.ShowDialog() == true)
+            {
+                downloadStationTextBox.Text = open.FileName;
+            }
+
+            // Green: #FF33B439
+            // Red: #FFAE0000
+            if (checkHash(open.FileName) == false && downloadStationTextBox.Text != "")
+            {
+                verifiedROM.Foreground = new SolidColorBrush(Color.FromRgb(0xB3, 0x00, 0x00));
+                verifiedROM.Content = "Bad ROM!";
+            }
+            else if (checkHash(open.FileName) == true)
+            {
+                verifiedROM.Foreground = new SolidColorBrush(Color.FromRgb(0x2D, 0x91, 0x39));
+                verifiedROM.Content = "Good ROM!";
+            }
+        }
+
+        private void bar_Quit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "NDS ROM (*.nds)|*.nds|All Files (*.*)|*.*";
+
+            if (save.ShowDialog() == true)
+            {
+                outputTextBox.Text = save.FileName;
+            }
+        }
+
+        private void addROMButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "All Usable Files (*.nds;*.srl)|*.nds;*.srl";
+            if (open.ShowDialog() == true)
+            {
+                romListBox.Items.Add(open.FileName);
+            }
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (createROM() == true)
+            {
+                MessageBox.Show("Patched ROM successfully saved to " + outputTextBox.Text + "!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
---
+++ dspatch/Program.cs
@@ -0,0 +1,130 @@
+﻿using dspatch.DS;
+using dspatch.IO;
+using dspatch.Nitro;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dspatch
+{
+    class Program
+    {
+        static byte[] dsHash = //Hash of the DS Download Station ROM
+        {
+            0xF1, 0x8B, 0x55, 0xF3, 0xE1, 0x25, 0x9C, 0x03, 0xE1, 0x0D, 0x0E, 0xCB,
+            0x54, 0x96, 0x93, 0xB4, 0x29, 0x05, 0xCE, 0xB5
+        };
+
+        static void PrintUsage() //Won't be used
+        {
+            Console.WriteLine("Usage: dspatch -s download_station.nds -o result.nds [-i rom1.nds] [-I romfolder1]");
+            Console.WriteLine("You can add multiple roms by using -I or -i multiple times");
+        }
+
+        static void Main(string[] args) //I will include credits
+        {
+            Console.WriteLine("== DS Download Station Patcher v1.0 ==");
+            Console.WriteLine("Exploit by Gericom, shutterbug2000 and Apache Thunder\n");
+
+            if (args.Length <= 1)
+            {
+                PrintUsage();
+                return;
+            }
+
+            string dsPath = null; //Init variable for the path to the DS Download Station ROM
+            string outPath = null; //Init variable for path to write the patched ROM to
+            List<string> romPaths = new List<string>(); //Init list of ROMs to include in the patched ROM
+            string filePath;
+            //parse arguments
+            int q = 0; //Unknown; Will look into later
+            while (q < args.Length - 1)
+            {
+                string arg = args[q++];
+                switch (arg)
+                {
+                    case "-s": //DS Download Station ROM
+                        filePath = args[q++];
+                        if (!File.Exists(filePath))
+                        {
+                            Console.WriteLine("Error: File (" + filePath + ") does not exist!");
+                            return;
+                        }
+                        dsPath = filePath;
+                        break;
+                    case "-o": //Output ROM Path
+                        outPath = args[q++];
+                        break;
+                    case "-i": //File to include in patched ROM
+                        filePath = args[q++];
+                        if (!File.Exists(filePath))
+                        {
+                            Console.WriteLine("Error: File (" + filePath + ") does not exist!");
+                            return;
+                        }
+                        romPaths.Add(filePath); //Add the file to the list of ROMs
+                        break;
+                    case "-I": //Directory; may not add to GUI, still may if I have time.
+                        string dirPath = args[q++];
+                        if (!Directory.Exists(dirPath))
+                        {
+                            Console.WriteLine("Error: Directory (" + dirPath + ") does not exist!");
+                            return;
+                        }
+                        romPaths.AddRange(Directory.GetFiles(dirPath, "*.nds"));
+                        romPaths.AddRange(Directory.GetFiles(dirPath, "*.srl"));
+                        break;
+                    default:
+                        Console.WriteLine("Error: Invalid argument (" + arg + ")\n");
+                        PrintUsage();
+                        return;
+                }
+            }
+
+            if (dsPath == null)
+            {
+                Console.WriteLine("Error: Specify a download station rom!\n");
+                PrintUsage();
+                return;
+            }
+            if (outPath == null)
+            {
+                Console.WriteLine("Error: Specify a destination path!\n");
+                PrintUsage();
+                return;
+            }
+            if (romPaths.Count == 0)
+            {
+                Console.WriteLine("Error: Specify at least 1 rom!\n");
+                PrintUsage();
+                return;
+            }
+
+            byte[] dsdata = File.ReadAllBytes(dsPath); //Read the DS Download Station ROM into an array of bytes
+            byte[] sha1 = SHA1.Create().ComputeHash(dsdata); //Determine the SHA1 hash of the ROM
+            //Compare the hash 20 times just in case a mistake is made
+            for (int i = 0; i < 20; i++)
+            {
+                if (sha1[i] != dsHash[i])
+                {
+                    Console.WriteLine("Error: Invalid download station rom!");
+                    Console.WriteLine("The patcher is only compatible with:");
+                    Console.WriteLine("xxxx - DS Download Station - Volume 1 (Kiosk WiFi Demo Cart) (U)(Independent).nds");
+                    Console.WriteLine("SHA1: F18B55F3E1259C03E10D0ECB549693B42905CEB5");
+                    return;
+                }
+            }
+            DownloadStationPatcher p = new DownloadStationPatcher(new NDS(dsdata)); //Use the patcher to create the patched ROM
+            foreach(var r in romPaths)
+                p.AddRom(new NDS(File.ReadAllBytes(r))); //Add all of the ROMs in the list of ROMs
+            byte[] finalResult = p.ProduceRom().Write(); //Write the bytes of the final, patched ROM
+            File.Create(outPath).Close(); //Create and then close the file
+            File.WriteAllBytes(outPath, finalResult); //Write the bytes of the patched ROM to the file for the patched ROM
+            //We are done!
+        }
+    }
+}
